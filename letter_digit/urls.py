29527c6b2dd67e4cf9f4c596df7d8b6a13b237f5
Aleksandr <aleksandr.kurilov@quandoo.com>
2020-06-02 02:41:42 +0200

    init commit

--- letter_digit/urls.py
+++ letter_digit/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('letter_digit', views.LetterCasePermutationView.as_view(), name='letter-permutation')
+    path('', views.LetterCasePermutationView.as_view(), name='letter-permutation')
 ]
